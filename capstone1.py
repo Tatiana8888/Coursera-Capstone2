8dafea5969c99ec72ce7281b25c0a99a4b1d6670
Tatiana8888 <ridstertat@gmail.com>
2020-01-01 18:20:29 +0300

    adds a python script

---
+++ capstone1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  This notebook will be mainly used for the capstone project!
+#  
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# # "Hello Capstone Project Course"
+
+# In[5]:
+
+
+print ("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
